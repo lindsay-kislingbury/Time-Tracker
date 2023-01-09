42fb3de09b877e48cbb1c6e7ba85c9d9e0c44775
lindsay-kislingbury <lindsaymoses@gmail.com>
2023-01-08 23:18:45 -0800

    ashboard layout completed

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -6,31 +6,51 @@ let msg = "valid";
 const loginCheck = (req, res) => {
     if(!req.user){
         msg = "valid";
-        return res.render('index', {message: msg});
+        return res.render('index', 
+        {
+            message: msg,
+            title: "Login"
+        });
     }
     else{
         const name=req.user.name;
-        res.render('dashboard', {user: name});
+        res.render('dashboard', 
+        {
+            user: name,
+            title: "Dashboard"
+        });
     }
 };
 
 //Invalid Login 
 const invalidLogin = (req, res) => {
     msg = "Incorrect Email or Password";
-    res.render('index', {message: msg});
+    res.render('index', 
+    {
+        message: msg,
+        title: "Login"
+    });
 }
 
 
 //Logged in dashboard
 const login = (req, res, next) => {
     const name= req.user.name;
-    res.render('dashboard', {user: name});
+    res.render('dashboard', 
+    {
+        user: name,
+        title: "Dashboard",
+    });
 };
 
 //Get register form
 const signupCheck = (req, res) => {
     msg = "valid";
-    res.render('register', {message: msg});
+    res.render('register', 
+    {
+        message: msg,
+        title: "Register"
+    });
 }
 
 //Create user
@@ -38,7 +58,11 @@ const signup = async (req, res) => {
     const {username, name, password} = req.body;
     if(req.body.password != req.body.confirmPassword){
         let msg = "confirm password does not match";
-        return res.render('register', {message: msg});
+        return res.render('register', 
+        {
+            message: msg,
+            title: "Register"
+        });
     }
     try{
         let user = await User.findOne({username});
@@ -48,11 +72,18 @@ const signup = async (req, res) => {
             return res.redirect('/');
         }
         let msg = "the user with this email already exists"
-        return res.render('register', {message: msg});
+        return res.render('register', 
+        {
+            message: msg,
+            title: "Register"
+        });
     }
     catch(error){
         msg = "some error occured";
-        return res.render('register', {message: msg});
+        return res.render('register', {
+            message: msg,
+            title: "Register"
+        });
     }
 };
 
---
+++ public/js/dashboard.js
@@ -0,0 +1,27 @@
+homeDiv = document.getElementById('home');
+statsDiv = document.getElementById('stats');
+settingsDiv = document.getElementById('settings');
+
+const showHome = () => {
+    settingsDiv.style.display = 'none';
+    statsDiv.style.display = 'none';
+    homeDiv.style.display = 'block';
+}
+
+const showStats = () => {
+    settingsDiv.style.display = 'none';
+    statsDiv.style.display = 'block';
+    homeDiv.style.display = 'none';
+}
+
+const showSettings = () => {
+    settingsDiv.style.display = 'block';
+    statsDiv.style.display = 'none';
+    homeDiv.style.display = 'none';
+}
+
+
+
+document.getElementById('homeButton').addEventListener('click', showHome);
+document.getElementById('settingsButton').addEventListener('click', showSettings);
+document.getElementById('statsButton').addEventListener('click', showStats);
