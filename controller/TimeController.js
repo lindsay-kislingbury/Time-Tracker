dd0947a40cff9ff4883520d9be01931f99a9183b
lindsay-kislingbury <lindsaymoses@gmail.com>
2023-05-22 22:21:59 -0700

    formatted date before storing

--- controller/TimeController.js
+++ controller/TimeController.js
@@ -1,31 +1,28 @@
 const mongoose = require('mongoose');
 const User = require('../models/User');
+const date = require('date-and-time');
 
 const stamp = async (req, res) => {
    console.log(req.body);
    const {startTime, stopTime, title, category} = req.body;
    let start = new Date(startTime);
    let end = new Date(stopTime);
-
    console.log("start time: ", start);
    console.log("end time: ", end);
-
-   
    let elapsedTime = date.subtract(end, start).toSeconds();
    console.log("elapsed time: ", elapsedTime);
+   let formattedDate = date.format(start, 'ddd MM/DD/YYYY');
 
    let timestamp = {
       title: title,
       category: category,
-      date: start,
+      date: formattedDate,
       elapsedTime: elapsedTime
    }
    
    let user = await User.findById(req.user.id);
-   
    user.timestamps.push(timestamp);
    await user.save();
-   
 };
 
 module.exports = stamp;
